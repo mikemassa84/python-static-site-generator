b8a9de837eac7db209fcc25f2362f2709d9ba513
Mike Massa <michael.massa@nm.org>
2020-06-30 15:34:05 -0500

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -8,7 +8,7 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest.joinpath(path.relative_to(self.source))  # self.dest + path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
 
         directory.mkdir(parents=True, exist_ok=True)
 
